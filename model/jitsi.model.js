dde7911e33ef7fa3fe88cb4241cdef640bf17708
Naveen <navee1110@gmail.com>
2025-02-18 12:13:30 +0530

    jitsi data updated

--- model/jitsi.model.js
+++ model/jitsi.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const jitsiDataSchema = new mongoose.Schema({
     name: {
       type: String,
-      required: true,
+   
     },
     timeStamp: {
       type: String,
